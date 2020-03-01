07041cb696f27652d39b344b245715fe9ce3785d
edoulaX <edou.lacroix@gmail.com>
2020-03-01 18:17:21 +0100

    update EclipticToEquatorialConversion

--- src/ch/epfl/rigel/coordinates/EclipticToEquatorialConversion.java
+++ src/ch/epfl/rigel/coordinates/EclipticToEquatorialConversion.java
@@ -1,5 +1,6 @@
 package ch.epfl.rigel.coordinates;
 
+import ch.epfl.rigel.astronomy.Epoch;
 import ch.epfl.rigel.math.Angle;
 import ch.epfl.rigel.math.Polynomial;
 
@@ -9,10 +10,15 @@ import java.util.function.Function;
 public final class EclipticToEquatorialConversion implements Function<EclipticCoordinates, EquatorialCoordinates> {
 
     // problem because Angle.ofDMS return radians and not minutes
-    private final static double lastCoef = Angle.ofDMS(23, 26, 21.45 );
+    private final static double LAST_COEF = Angle.ofDMS(23, 26, 21.45 );
     private final Polynomial epsilonPolynomial = Polynomial.of( 0.00181, -0.0006, -46.815, 0 );
-
+    private double cosEpsilon;
+    private double sinEpsilon;
     public EclipticToEquatorialConversion( ZonedDateTime when ) {
+        double daysT = Epoch.J2000.daysUntil( when );
+        double epsilon = Angle.ofDeg( epsilonPolynomial.at(daysT) ) + LAST_COEF;
+        cosEpsilon = Math.cos( epsilon );
+        sinEpsilon = Math.sin( epsilon );
         // changement de système de coordonnées entre les coordonnées écliptiques et les coordonnées équatoriales pour le couple date/heure when.
         // ca veut dire quoi lol ?
     }
@@ -20,16 +26,17 @@ public final class EclipticToEquatorialConversion implements Function<EclipticCo
     @Override
     public EquatorialCoordinates apply( EclipticCoordinates ecl )
     {
-        double lambda = ecl.lon();
-        double beta = ecl.lat();
-        System.out.println(epsilonPolynomial);
+        double sinLambda = Math.sin( ecl.lon() );
+        double cosLambda = Math.cos( ecl.lon() );
 
-        // need to do a conversion from the polynomial to an actual angle !!!
+        double sinBeta = Math.sin( ecl.lat() );
+        double cosBeta = Math.cos( ecl.lat() );
+        double tanBeta = Math.tan( ecl.lat() );
 
-        //double T = Epoch.J2000.julianCenturiesUntil( ???????? );
-        //double epsilon = epsilonPolynomial.at( T )
-        //double alpha = Math.atan2( ( Math.sin( lambda ) * Math.cos( epsilon ) - Math.tan( beta ) * Math.sin( epsilon ) ) / Math.cos( lambda ) );
-        return null;
+        double lon = Math.atan2((sinLambda * cosEpsilon - tanBeta * sinEpsilon) / cosLambda, 1);
+        double lat = Math.asin(sinBeta * cosEpsilon + cosBeta * sinEpsilon * sinLambda);
+
+        return EquatorialCoordinates.of(lon, lat);
     }
 
     @Override
