25af6bcf56aaad5f0adee7c03da176395accfab0
PeacefulOtter <zankro67@gmail.com>
2020-03-02 13:49:53 +0100

    improvements on conversion

--- test/ch/epfl/sigcheck_part3/SignatureChecks_3.java
+++ test/ch/epfl/sigcheck_part3/SignatureChecks_3.java
@@ -1,28 +1,43 @@
-package ch.epfl.sigcheck;
+
+package ch.epfl.sigcheck_part3;
 
 import ch.epfl.rigel.astronomy.Epoch;
 import ch.epfl.rigel.astronomy.SiderealTime;
 import ch.epfl.rigel.coordinates.*;
+import org.junit.jupiter.api.Test;
 
-import java.time.ZonedDateTime;
+import java.time.*;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.function.Function;
+
+import static org.junit.Assert.assertEquals;
 
 final class SignatureChecks_3 {
     void checkEpoch() {
         double d;
-        ZonedDateTime z = null;
+        ZonedDateTime z = ZonedDateTime.of( 1980, 4, 22, 14, 36, 51, 670000000, ZoneId.of( "UT" ) );
+        System.out.println(z);
+
         Epoch e = Epoch.J2000;
-        e = Epoch.J2010;
         d = e.daysUntil(z);
+        //assertEquals(  )
         d = e.julianCenturiesUntil(z);
+        e = Epoch.J2010;
+
     }
 
+    @Test
     void checkSiderealTime() {
         double d;
-        ZonedDateTime z = null;
-        GeographicCoordinates g = null;
+        ZoneId utc = ZoneId.of( ZoneOffset.UTC.getId() );
+        ZonedDateTime z = ZonedDateTime.of( 1980, 4, 22, 14, 36, 51, 670000000, utc );
+
+        //GeographicCoordinates g = null;
         d = SiderealTime.greenwich(z);
-        d = SiderealTime.local(z, g);
+        System.out.println(d);
+        //assertEquals( );
+        //d = SiderealTime.local(z, g);
     }
 
     void checkEclipticToEquatorialConversion() {
