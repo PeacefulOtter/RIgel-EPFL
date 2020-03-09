6a275d88953e7f079207dcb77a86e8a44e09d66f
edoulaX <edou.lacroix@gmail.com>
2020-03-09 14:26:37 +0100

    update Stereographihc Apply inverseAplly

--- src/ch/epfl/rigel/coordinates/StereographicProjection.java
+++ src/ch/epfl/rigel/coordinates/StereographicProjection.java
@@ -8,38 +8,38 @@ public final class StereographicProjection implements Function<HorizontalCoordin
     private final HorizontalCoordinates center;
     private final double lambda0;
     private final double phi1;
-    private final double cosLambda;
-    private final double sinLambda;
-    private final double cosPhi;
-    private final double sinPhi;
+    private final double cosLambda0;
+    private final double sinLambda0;
+    private final double cosPhi1;
+    private final double sinPhi1;
 
     public StereographicProjection( HorizontalCoordinates center )
     {
         this.center = center;
         lambda0 = center.lon();
         phi1 = center.lat();
-        cosLambda = Math.cos( lambda0 );
-        sinLambda = Math.sin( lambda0 );
-        cosPhi = Math.cos( phi1 );
-        sinPhi = Math.sin( phi1 );
+        cosLambda0 = Math.cos( lambda0 );
+        sinLambda0 = Math.sin( lambda0 );
+        cosPhi1 = Math.cos( phi1 );
+        sinPhi1 = Math.sin( phi1 );
         // /!\ Calculate as much values as possible
-    }
-
-    @Override
-    public CartesianCoordinates apply( HorizontalCoordinates azAlt )
-    {
-        return null;
     }
 
     public CartesianCoordinates circleCenterForParallel( HorizontalCoordinates hor )
     {
-        double cy = cosPhi / ( Math.sin( hor.alt() ) + sinPhi );
+        if ( ( Math.sin( hor.alt() ) + sinPhi1 ) == 0){
+            return CartesianCoordinates.of(0, Double.POSITIVE_INFINITY); // a voir coordonne infinie ??
+        }
+        double cy = cosPhi1 / ( Math.sin( hor.alt() ) + sinPhi1 );
         return CartesianCoordinates.of( 0, cy );
     }
 
     public double circleRadiusForParallel( HorizontalCoordinates parallel )
     {
-        return Math.cos( parallel.lat() ) / ( Math.sin( parallel.lon() ) + sinPhi );
+        if ( Math.sin( parallel.lon() ) + sinPhi1 == 0){
+            return Double.POSITIVE_INFINITY;
+        }
+        return Math.cos( parallel.lat() ) / ( Math.sin( parallel.lon() ) + sinPhi1 );
     }
 
     public double applyToAngle( double rad )
@@ -47,9 +47,35 @@ public final class StereographicProjection implements Function<HorizontalCoordin
         return 2 * Math.tan( rad / 4 );
     }
 
+    @Override
+    public CartesianCoordinates apply( HorizontalCoordinates azAlt )
+    {
+        double cosLambda = Math.cos( azAlt.alt() );
+        double sinLambda = Math.sin( azAlt.alt() );
+        double cosPhi = Math.cos( azAlt.az() );
+        double sinPhi = Math.sin( azAlt.az() );
+
+        double d = 1 / (1 + sinLambda * sinPhi1 + cosPhi * cosPhi1 * Math.cos(azAlt.alt() - lambda0));
+        // peut etre mettre une valeur infinie
+
+        double x = d * cosPhi * Math.sin(azAlt.alt() - lambda0);
+        double y = d * (sinPhi * cosPhi1 - cosPhi * sinPhi1 * Math.cos(azAlt.alt() - lambda0));
+
+        return CartesianCoordinates.of( x, y );
+    }
+
     public HorizontalCoordinates inverseApply( CartesianCoordinates xy )
     {
-        return null;
+        double p = Math.sqrt( Math.pow(xy.x(), 2) + Math.pow(xy.y(), 2) );
+        double cosC = (1 - Math.pow(p, 2)) / (Math.pow(p, 2) + 1);
+        double sinC = (2 * p) / (Math.pow(p, 2) + 1) ;
+        double x = xy.x();
+        double y = xy.y();
+
+        double lambda = Math.atan2(x * sinC, p * cosPhi1 * cosC - y * sinPhi1 * sinC) + lambda0;
+        double phi = Math.asin(cosC * sinPhi1 + (y * sinC * cosPhi1 / p));
+
+        return HorizontalCoordinates.of(lambda, phi);
     }
 
     @Override
