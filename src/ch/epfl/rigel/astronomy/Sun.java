fec1f9b64b5981ed5ee5be9decaee16e0c287e61
PeacefulOtter <zankro67@gmail.com>
2020-03-09 18:56:28 +0100

    wtf is this project

--- src/ch/epfl/rigel/astronomy/Sun.java
+++ src/ch/epfl/rigel/astronomy/Sun.java
@@ -2,11 +2,16 @@ package ch.epfl.rigel.astronomy;
 
 import ch.epfl.rigel.coordinates.EclipticCoordinates;
 import ch.epfl.rigel.coordinates.EquatorialCoordinates;
+import ch.epfl.rigel.math.Angle;
 
 public final class Sun extends CelestialObject
 {
-    // Sun magnitude constant
-    private final static float magnitude = -26.7f;
+    private static final float MAGNITUDE = -26.7f;
+    //private static final double ANGULAR_SPEED = Angle.TAU / 365.242191;
+    //private static final double LON_J2010 = Angle.ofDeg( 279.557208 );
+    //private static final double LON_PERIGEE = Angle.ofDeg( 283.112438 );
+    //private static final double ECCENTRICITY = Angle.ofArcsec( 0.016705 );
+
 
     // save the ecliptic position and the mean anomaly
     private final EclipticCoordinates eclipticPos;
@@ -14,7 +19,7 @@ public final class Sun extends CelestialObject
 
     public Sun( EclipticCoordinates eclipticPos, EquatorialCoordinates equatorialPos, float angularSize, float meanAnomaly )
     {
-        super( "Soleil", equatorialPos, angularSize, magnitude );
+        super( "Soleil", equatorialPos, angularSize, MAGNITUDE );
         if ( eclipticPos == null )
         {
             throw new NullPointerException( "EclipticPos is NotNull" );
--- src/ch/epfl/rigel/astronomy/SunModel.java
+++ src/ch/epfl/rigel/astronomy/SunModel.java
@@ -2,17 +2,22 @@ package ch.epfl.rigel.astronomy;
 
 import ch.epfl.rigel.coordinates.EclipticToEquatorialConversion;
 
+import javax.naming.ServiceUnavailableException;
+
 public enum SunModel implements CelestialObjectModel<Sun> {
 
     SUN();
 
-    private SunModel() {
-
+    private SunModel()
+    {
     }
 
     @Override
-    public Sun at( double daysSinceJ2010, EclipticToEquatorialConversion eclipticToEquatorialConversion )
+    public Sun at(
+            double daysSinceJ2010,
+            EclipticToEquatorialConversion eclipticToEquatorialConversion )
     {
+        // eclipticToEquatorialConversion.apply( EclipticCoordinates )
         return null;
     }
 }
