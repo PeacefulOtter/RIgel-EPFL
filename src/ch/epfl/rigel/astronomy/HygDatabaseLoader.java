57fd2ed89ed7ac1fa12b3f7410d988f93740fc31
PeacefulOtter <zankro67@gmail.com>
2020-03-30 18:23:44 +0200

    completed part of the load method of HygDatabaseLoader

--- src/ch/epfl/rigel/astronomy/HygDatabaseLoader.java
+++ src/ch/epfl/rigel/astronomy/HygDatabaseLoader.java
@@ -1,13 +1,26 @@
 package ch.epfl.rigel.astronomy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static java.nio.charset.StandardCharsets.US_ASCII;
 
 public enum HygDatabaseLoader implements StarCatalogue.Loader {
 
     INSTANCE();
 
     @Override
-    public void load(InputStream inputStream, StarCatalogue.Builder builder) {
+    public void load( InputStream inputStream, StarCatalogue.Builder builder ) {
+        // hip, proper, (or bayer+con), rarad decrad, mag || 0, ci || 0
 
+        try ( BufferedReader stream = new BufferedReader( new InputStreamReader( inputStream, US_ASCII ) ) )
+        {
+            // stream.readLine
+        } catch ( IOException e )
+        {
+            e.printStackTrace();
+        }
     }
 }
