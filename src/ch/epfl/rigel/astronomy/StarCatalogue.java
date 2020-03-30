55ce39978b31eb2311da58fdc7ea7576564c3aaf
PeacefulOtter <zankro67@gmail.com>
2020-03-30 17:19:24 +0200

    improved starCatalogue

--- src/ch/epfl/rigel/astronomy/StarCatalogue.java
+++ src/ch/epfl/rigel/astronomy/StarCatalogue.java
@@ -1,33 +1,36 @@
 package ch.epfl.rigel.astronomy;
 
+import ch.epfl.rigel.Preconditions;
+
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
-public final class StarCatalogue {
+public final class StarCatalogue
+{
 
-    private HashMap<Asterism, List<Integer>> index;
+    private Map<Asterism, List<Integer>> index;
     private List<Star> stars;
     private Set<Asterism> asterisms;
 
-    public StarCatalogue(List<Star> stars, List<Asterism> asterisms){
-        for ( Asterism asterism : asterisms ) {
-            if (!stars.contains(asterism.stars())){
-                throw new IllegalArgumentException();
-            }
+    public StarCatalogue( List<Star> stars, List<Asterism> asterisms )
+    {
+        for ( Asterism asterism : asterisms )
+        {
+            Preconditions.checkArgument( stars.contains( asterism.stars() ) );
         }
 
         this.stars = stars;
-        this.asterisms = Set.copyOf(asterisms);
+        this.asterisms = Set.copyOf( asterisms );
+        this.index = new HashMap<>();
 
-        for (Asterism asterism : asterisms) {
-            List< Integer > list = new ArrayList<>();
-            for ( Star asterismStar : asterism.stars() ) {
-                list.add(stars.indexOf(asterismStar));
+        for ( Asterism asterism : asterisms )
+        {
+            List<Integer> tempList = new ArrayList<>();
+            for ( Star asterismStar : asterism.stars() )
+            {
+                tempList.add( stars.indexOf( asterismStar ) );
             }
-            index.put(asterism, list);
+            index.put( asterism, tempList );
         }
     }
 
@@ -39,51 +42,52 @@ public final class StarCatalogue {
         return asterisms;
     }
 
-    public List<Integer> asterismIndices(Asterism asterism){
-        if ( !asterisms.contains(asterism)){
-            throw new IllegalArgumentException();
-        }
-
+    public List<Integer> asterismIndices( Asterism asterism )
+    {
+        Preconditions.checkArgument( asterisms.contains( asterism ) );
         return List.copyOf( index.get( asterism ) ) ;
     }
 
-    public final static class Builder {
+    public final static class Builder
+    {
 
-        Builder(){
+        Builder()
+        {
 
         }
 
-        Builder addStar(Star star){
+        Builder addStar( Star star ){
             return null;
         }
 
         // pas sur que ca soit ca qu il faut retourner ( une vue non modifiable )
-        List<Star> stars(){
+        List<Star> stars()
+        {
            // return Collections.unmodifiableList(stars());
             return null;
         }
 
-        Builder addAsterism(Asterism asterism){
+        Builder addAsterism( Asterism asterism ){
             return null;
         }
 
-        List<Asterism> asterisms(){
+        List<Asterism> asterisms()
+        {
             // return Collections.unmodifiableList(asterism());
             return null;
         }
 
-        Builder loadFrom(InputStream inputStream, Loader loader){
+        Builder loadFrom( InputStream inputStream, Loader loader ){
             return null;
         }
 
-        StarCatalogue build( ){
+        StarCatalogue build(){
             return null;
         }
     }
 
-    interface Loader {
-
-        public void load(InputStream inputStream, Builder builder);
-
+    interface Loader
+    {
+        public void load( InputStream inputStream, Builder builder );
     }
 }
