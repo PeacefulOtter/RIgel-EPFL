274a432b89879d14ea206eb6f8869416ef9198c3
edoulaX <edou.lacroix@gmail.com>
2020-04-02 12:57:28 +0200

    updated AsterismLoader

--- src/ch/epfl/rigel/astronomy/AsterismLoader.java
+++ src/ch/epfl/rigel/astronomy/AsterismLoader.java
@@ -1,12 +1,38 @@
 package ch.epfl.rigel.astronomy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.nio.charset.StandardCharsets.US_ASCII;
 
 public enum AsterismLoader implements StarCatalogue.Loader{
     INSTANCE();
 
     @Override
-    public void load(InputStream inputStream, StarCatalogue.Builder builder) {
+    public void load(InputStream inputStream, StarCatalogue.Builder builder) throws IOException
+    {
+        try ( BufferedReader stream = new BufferedReader( new InputStreamReader( inputStream, US_ASCII ) ) )
+        {
+            String line ; 
+            String[] starInfo;
 
+            while( ( line = stream.readLine() ) != null )
+            {
+                List<Star> asterism = new ArrayList<>();
+                starInfo = line.split( "," );
+                for ( String hip : starInfo ) {
+                    for ( Star star : builder.stars() ) {
+                        if( star.hipparcosId() == Integer.parseInt(hip)){
+                            asterism.add(star);
+                        }
+                    }
+                }
+                builder.addAsterism(new Asterism(asterism));
+            }
+        }
     }
 }
