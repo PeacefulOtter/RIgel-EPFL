46bc79f7728fd4d44348a3216912aa87d596745e
PeacefulOtter <zankro67@gmail.com>
2020-04-05 14:17:46 +0200

    added BlackBodyColor and tests for part 7

---
+++ src/ch/epfl/rigel/gui/BlackBodyColor.java
@@ -0,0 +1,44 @@
+package ch.epfl.rigel.gui;
+
+import ch.epfl.rigel.Preconditions;
+import ch.epfl.rigel.math.ClosedInterval;
+import javafx.scene.paint.Color;
+
+import java.io.*;
+
+public final class BlackBodyColor
+{
+    private static final String COLOR_FILE_NAME = "/bbr_color.txt";
+    private static final ClosedInterval KELVIN_INTERVAL = ClosedInterval.of( 1000, 40000 );
+
+    public static Color colorForTemperature( int kelvin )
+    {
+        Preconditions.checkInInterval( KELVIN_INTERVAL, kelvin );
+
+        String line;
+        String[] lineSplit;
+        String hexColor = "";
+
+        try ( BufferedReader stream = new BufferedReader( new InputStreamReader( BlackBodyColor.class.getResourceAsStream( COLOR_FILE_NAME ) ) ) )
+        {
+            while ( ( line = stream.readLine() ) != null )
+            {
+                lineSplit = line.split( "\\s+" );
+
+                if ( lineSplit[ 0 ].equals( "#" ) || lineSplit[ 3 ].equals( "2deg" ) ) continue;
+
+                if ( Integer.parseInt( lineSplit[ 1 ] ) == kelvin )
+                {
+                    hexColor = lineSplit[ 13 ];
+                    break;
+                }
+            }
+        }
+        catch( IOException e )
+        {
+            throw new UncheckedIOException( e );
+        }
+        
+        return Color.web( hexColor );
+    }
+}
---
+++ test/ch/epfl/sigcheck_part7/SignatureCheck_7.java
@@ -0,0 +1,19 @@
+package ch.epfl.sigcheck_part7;
+
+import ch.epfl.rigel.gui.BlackBodyColor;
+import org.junit.jupiter.api.Test;
+
+final class SignatureCheck_7
+{
+    @Test
+    void BlackBodyColorTest()
+    {
+        BlackBodyColor.colorForTemperature( 7000 );
+    }
+
+    @Test
+    void ObservedSkyTest()
+    {
+
+    }
+}
