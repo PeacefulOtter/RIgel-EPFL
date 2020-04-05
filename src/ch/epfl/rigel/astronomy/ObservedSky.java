2f175b6f3f718b0b372c863411fd1910a0dc98b5
edoulaX <edou.lacroix@gmail.com>
2020-04-05 15:19:22 +0200

    finished ObservedSky

--- src/ch/epfl/rigel/astronomy/ObservedSky.java
+++ src/ch/epfl/rigel/astronomy/ObservedSky.java
@@ -1,19 +1,16 @@
 
 package ch.epfl.rigel.astronomy;
 
-import ch.epfl.rigel.coordinates.CartesianCoordinates;
-import ch.epfl.rigel.coordinates.EclipticToEquatorialConversion;
-import ch.epfl.rigel.coordinates.GeographicCoordinates;
-import ch.epfl.rigel.coordinates.StereographicProjection;
+import ch.epfl.rigel.coordinates.*;
 
 import java.time.ZonedDateTime;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class ObservedSky
 {
-    private final List<PlanetModel> planetsWithoutEarth;
+    private final List< PlanetModel > planetsModelWithoutEarth;
+    private final List< Planet > planetsWithoutEarth;
+    private final HashMap< CelestialObject, CartesianCoordinates > planetCartesianCoordinates;
     private final Set<CelestialObject> celestialObjects;
     private final Sun sun;
     private final Moon moon;
@@ -24,52 +21,70 @@ public class ObservedSky
             StereographicProjection projection,
             StarCatalogue catalogue )
     {
-        planetsWithoutEarth = PlanetModel.ALL;
-        planetsWithoutEarth.remove( PlanetModel.EARTH );
+        planetsWithoutEarth = new ArrayList<>();
+        planetCartesianCoordinates = new HashMap<>();
+        planetsModelWithoutEarth = PlanetModel.ALL;
+        planetsModelWithoutEarth.remove( PlanetModel.EARTH );
         celestialObjects = new HashSet<>();
 
-        EclipticToEquatorialConversion conversion = new EclipticToEquatorialConversion( moment );
+        EclipticToEquatorialConversion conversionToEquatorial = new EclipticToEquatorialConversion( moment );
+        EquatorialToHorizontalConversion conversionToHorizontal = new EquatorialToHorizontalConversion( moment, position );
         double daysUntil = Epoch.J2000.daysUntil( moment );
 
-        sun = SunModel.SUN.at( daysUntil, conversion );
-        moon = MoonModel.MOON.at( daysUntil, conversion );
+        sun = SunModel.SUN.at( daysUntil, conversionToEquatorial );
+        moon = MoonModel.MOON.at( daysUntil, conversionToEquatorial );
         celestialObjects.add( sun );
         celestialObjects.add( moon );
 
-        for ( PlanetModel planet: planetsWithoutEarth)
+        for ( PlanetModel planet: planetsModelWithoutEarth)
         {
-            celestialObjects.add( planet.at( daysUntil, conversion ) );
+            Planet solarPlanet = planet.at( daysUntil, conversionToEquatorial );
+            celestialObjects.add( solarPlanet );
+            planetsWithoutEarth.add( solarPlanet );
         }
         for ( Star star : catalogue.stars() )
         {
             celestialObjects.add( star );
         }
+
+        for (CelestialObject planet :celestialObjects) {
+            planetCartesianCoordinates.put( planet, projection.apply(conversionToHorizontal.apply( planet.equatorialPos() ) ));
+        }
     }
 
 
     public Sun sun(){
-        return new Sun(null , null, 0, 0);
+        return sun;
     }
 
     public CartesianCoordinates sunPosition(){
-        return null;
+        return planetCartesianCoordinates.get(sun);
     }
 
     public Moon moon()
     {
-        return null;
+        return moon;
     }
 
     public CartesianCoordinates moonPosition()
     {
-        return null;
+        return planetCartesianCoordinates.get(moon);
     }
 
     public List< Planet > planets() {
-        return null;
+
+        return planetsWithoutEarth;
     }
 
     public double[] planetPosition(){
-        return null;
+        double[] cartesianCoordinates = new double[ 14 ];
+        int i = 0;
+        for (Planet planet : planetsWithoutEarth ) {
+            cartesianCoordinates[i] = planetCartesianCoordinates.get(planet).x();
+            i++;
+            cartesianCoordinates[i] = planetCartesianCoordinates.get(planet).y();
+            i++;
+        }
+        return cartesianCoordinates;
     }
 }
