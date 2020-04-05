04603e73684344fc7bccca0b767383464e8d656a
edoulaX <edou.lacroix@gmail.com>
2020-04-05 14:32:37 +0200

    updated ObservedSky

--- src/ch/epfl/rigel/astronomy/ObservedSky.java
+++ src/ch/epfl/rigel/astronomy/ObservedSky.java
@@ -7,22 +7,37 @@ import ch.epfl.rigel.coordinates.GeographicCoordinates;
 import ch.epfl.rigel.coordinates.StereographicProjection;
 
 import java.time.ZonedDateTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class ObservedSky {
 
     // pas sur de cette atribut
-    private Set< CelestialObject > celestObject;
+    private final Set< CelestialObject > celestObject;
+    private final Sun sun;
+    private final Moon moon;
 
     public ObservedSky( ZonedDateTime moment, GeographicCoordinates position,
-                        StereographicProjection projection, StarCatalogue catalogue ){
-        EclipticToEquatorialConversion p = new EclipticToEquatorialConversion(moment);
-        double m = Epoch.J2000.daysUntil( moment );
-        Sun sun = SunModel.SUN.at(m, p);
-        //ce constructeur calcule la position projetée dans le plan de tous
-        //les objets célestes, à savoir : le Soleil, la Lune, les planètes du système solaire — à l'exception de la Terre — et les étoiles du catalogue.
+                        StereographicProjection projection, StarCatalogue catalogue ) {
+        celestObject = new HashSet<>();
+        EclipticToEquatorialConversion p = new EclipticToEquatorialConversion( moment );
+        double m = Epoch.J2000.daysUntil(moment);
+
+        sun = SunModel.SUN.at(m, p);
+        moon = MoonModel.MOON.at(m, p);
+        celestObject.add(sun);
+        celestObject.add(moon);
+        for ( PlanetModel planet: PlanetModel.ALL ) {
+            if ( planet.equals(PlanetModel.EARTH)) continue;
+            celestObject.add(planet.at(m, p));
+        }
+
+        for (Star star : catalogue.stars()) {
+            celestObject.add(star);
+        }
     }
+
     public Sun sun(){
         return new Sun(null , null, 0, 0);
     }
