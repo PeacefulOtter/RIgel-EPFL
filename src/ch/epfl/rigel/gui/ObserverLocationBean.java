504708b1f0efe517f8971348ffdef88a85870e15
edoulaX <edou.lacroix@gmail.com>
2020-05-01 15:14:05 +0200

    part 10  end ObservedLocationBean and start SkyCanvasManager

--- src/ch/epfl/rigel/gui/ObserverLocationBean.java
+++ src/ch/epfl/rigel/gui/ObserverLocationBean.java
@@ -1,10 +1,44 @@
 package ch.epfl.rigel.gui;
 
 import ch.epfl.rigel.coordinates.GeographicCoordinates;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.value.ObservableObjectValue;
 
 public class ObserverLocationBean
 {
-    public void setCoordinates( GeographicCoordinates ofDeg )
-    {
+    private DoubleProperty latDeg, lonDeg;
+
+    private ObservableObjectValue<GeographicCoordinates> coordinates;
+    // peut etre pas mettre de constructeur et mettre des setter comme dans viewingParameters
+    public ObserverLocationBean(double lonDeg, double latDeg) {
+        this.latDeg = new SimpleDoubleProperty( latDeg );
+        this.lonDeg = new SimpleDoubleProperty( lonDeg );
+        coordinates = Bindings.createObjectBinding(() -> GeographicCoordinates.ofDeg(lonDeg, latDeg), this.lonDeg, this.latDeg);
+    }
+
+    public double getLatDeg() {
+        return latDeg.get();
+    }
+
+    public DoubleProperty latDegProperty() {
+        return latDeg;
+    }
+
+    public double getLonDeg() {
+        return lonDeg.get();
+    }
+
+    public DoubleProperty lonDegProperty() {
+        return lonDeg;
+    }
+
+    public GeographicCoordinates getCoordinates() {
+        return coordinates.get();
+    }
+
+    public ObservableObjectValue coordinatesProperty() {
+        return coordinates;
     }
 }
--- src/ch/epfl/rigel/gui/SkyCanvasManager.java
+++ src/ch/epfl/rigel/gui/SkyCanvasManager.java
@@ -1,17 +1,25 @@
 package ch.epfl.rigel.gui;
 
+import ch.epfl.rigel.astronomy.CelestialObject;
 import ch.epfl.rigel.astronomy.StarCatalogue;
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.value.ObservableObjectValue;
 import javafx.scene.canvas.Canvas;
 
 public class SkyCanvasManager
 {
+    public DoubleProperty mouseAzDeg, mouseAltDeg;
+    public ObservableObjectValue<CelestialObject> objectUnderMouse ;
+
     public SkyCanvasManager(
             StarCatalogue catalogue,
             DateTimeBean dateTimeBean,
             ObserverLocationBean observerLocationBean,
             ViewingParametersBean viewingParametersBean )
     {
+        Canvas canvas = new Canvas( 800, 600 );
+        SkyCanvasPainter painter = new SkyCanvasPainter( canvas );
     }
 
     public ObjectProperty objectUnderMouseProperty()
