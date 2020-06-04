e48bd816a3cdad3d88617d39f6998b80b79a7265
Nathan <zankro67@gmail.com>
2020-06-04 19:20:06 +0200

    fixed saturne card

--- src/ch/epfl/rigel/astronomy/SolarSystemInfo.java
+++ src/ch/epfl/rigel/astronomy/SolarSystemInfo.java
@@ -11,7 +11,7 @@ public enum SolarSystemInfo
     VENUS(   "Vénus",   "venus.jpg",  "Planète tellurique", 490,   446, 42,  6052,  8.9,  460 ),
     MARS(    "Mars",    "mars.jpg",   "Planète tellurique", 36,   -140, 78,  3389,  3.7,  145 ),
     JUPITER( "Jupiter", "jupiter.jpg","Planète gazeuse",   -118,  -129, 628, 69911, 24.8, 61420 ),
-    SATURN(  "Saturn",  "saturn.jpg", "Planète gazeuse",   -139,  -201, 1275,58232, 10.4 ,42700 ),
+    SATURN(  "Saturne", "saturn.jpg", "Planète gazeuse",   -139,  -201, 1275,58232, 10.4 ,42700 ),
     URANUS(  "Uranus",  "uranus.jpg", "Planète gazeuse",   -208 , -212 ,2723,25362 ,8.9 , 8083 ),
     NEPTUNE( "Neptune", "neptune.jpg","Planète gazeuse",   -210,  -220, 4351,24622 ,11.2 ,7618 );
 
