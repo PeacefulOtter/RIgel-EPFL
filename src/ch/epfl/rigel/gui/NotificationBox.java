6ef99c9df91b7a504a7b2a4d2a045444687c8a64
Nathan <zankro67@gmail.com>
2020-06-04 15:29:31 +0200

    idk

--- src/ch/epfl/rigel/gui/NotificationBox.java
+++ src/ch/epfl/rigel/gui/NotificationBox.java
@@ -1,5 +1,6 @@
 package ch.epfl.rigel.gui;
 
+import javafx.animation.FadeTransition;
 import javafx.animation.TranslateTransition;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
@@ -12,22 +13,20 @@ import javafx.util.Duration;
 
 public class NotificationBox extends HBox
 {
-    private static final Duration TRANSLATION_DURATION = Duration.millis( 1500 );
-    private static final TranslateTransition TRANSLATE_IN_TRANSITION = new TranslateTransition( TRANSLATION_DURATION );
-    private static final TranslateTransition TRANSLATE_OUT_TRANSITION = new TranslateTransition( TRANSLATION_DURATION );
-
+    private static final Duration TRANSITION_DURATION = Duration.millis( 1500 );
+    private static final FadeTransition FADE_IN_TRANSITION = new FadeTransition( TRANSITION_DURATION );
+    private static final FadeTransition FADE_OUT_TRANSITION = new FadeTransition( TRANSITION_DURATION );
     private static final Font FONT_AWESOME = new FontLoader().loadFontAwesome();
 
-    private final ImageView imgView;
     private static final String SUCCESS_IMG_PATH = "success.png";
     private static final String ERROR_IMG_PATH = "error.png";
 
-    private static final double LOGO_WIDTH = 21;
     private static final double LETTER_WIDTH = 9;
     private static final double NOTIFICATION_HEIGHT = 30;
 
+    private final ImageView imgView;
     private Pane parentElt;
-    private int notificationWidth;
+    private int notificationWidth = 0;
 
     NotificationBox()
     {
@@ -40,8 +39,20 @@ public class NotificationBox extends HBox
                 "-fx-border-radius: 5px;" +
                 "-fx-background-color: #ffffff;" +
                 "-fx-effect: dropshadow(three-pass-box, rgba(0, 0, 0, 0.8), 10, 0, 0, 0);" );
-        TRANSLATE_IN_TRANSITION.setNode( this );
-        TRANSLATE_OUT_TRANSITION.setNode( this );
+        FADE_IN_TRANSITION.setNode( this );
+        FADE_OUT_TRANSITION.setNode( this );
+        FADE_IN_TRANSITION.setFromValue( 0 );
+        FADE_IN_TRANSITION.setToValue( 1 );
+        FADE_OUT_TRANSITION.setFromValue( 1 );
+        FADE_OUT_TRANSITION.setToValue( 0 );
+
+        FADE_IN_TRANSITION.setOnFinished( event ->
+                FADE_OUT_TRANSITION.play()
+        );
+        
+        FADE_OUT_TRANSITION.setOnFinished( event ->
+                parentElt.getChildren().remove( this )
+        );
 
         imgView = new ImageView();
         imgView.setX( 0 );
@@ -59,7 +70,7 @@ public class NotificationBox extends HBox
             Image img = new Image( "/img/" + imgPath );
             imgView.setImage( img );
             this.getChildren().add( imgView );
-            notificationWidth += NOTIFICATION_HEIGHT;
+            notificationWidth += (int) NOTIFICATION_HEIGHT;
         }
         return this;
     }
@@ -86,21 +97,13 @@ public class NotificationBox extends HBox
 
     public void fire()
     {
+        if ( parentElt == null ) { throw new NullPointerException( "No Parent Element has been set" ); }
         this.setPrefWidth( notificationWidth );
-        parentElt.getChildren().add( this );
 
         double parentWidth = parentElt.getWidth();
-        TRANSLATE_IN_TRANSITION.setFromX( parentWidth );
-        TRANSLATE_IN_TRANSITION.setToX( parentWidth - notificationWidth );
-        TRANSLATE_OUT_TRANSITION.setFromX( parentWidth - notificationWidth );
-        TRANSLATE_OUT_TRANSITION.setToX( parentWidth );
+        this.setLayoutX( parentWidth - notificationWidth );
+        parentElt.getChildren().add( this );
 
-        TRANSLATE_IN_TRANSITION.play();
-        TRANSLATE_IN_TRANSITION.setOnFinished(  event ->
-            TRANSLATE_OUT_TRANSITION.play()
-        );
-        TRANSLATE_OUT_TRANSITION.setOnFinished( event ->
-            parentElt.getChildren().remove( this )
-        );
+        FADE_IN_TRANSITION.play();
     }
 }
